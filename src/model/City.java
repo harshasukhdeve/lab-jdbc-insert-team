0a261c67e5564fd55944d7f976cd15cc96002589
harshasukhdeve <harshasukhdeve0903@gmail.com>
2020-05-27 16:47:36 +0530

    FSJ0420A115

--- src/model/City.java
+++ src/model/City.java
@@ -1,1 +1,26 @@
+package model;
+
+public class City{
+	long cityid;
+	String cityName;
+	public long getCityid() {
+		return cityid;
+	}
+	public void setCityid(long cityid) {
+		this.cityid = cityid;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public City(long cityld,String cityName) {
+		this.cityid=cityid;
+		this.cityName=cityName;
+	}
+	
+	
+}
+
 
--- src/model/Team.java
+++ src/model/Team.java
@@ -1,1 +1,43 @@
+package model;
 
+public class Team{
+	
+	String fname;
+	String coach; 
+	String captain;
+	City city;
+	public String getName() {
+		return fname;
+	}
+	public void setName(String fname) {
+		this.fname = fname;
+	}
+	public String getCoach() {
+		return coach;
+	}
+	public void setCoach(String coach) {
+		this.coach = coach;
+	}
+	public String getCaptain() {
+		return captain;
+	}
+	public void setCaptain(String captain) {
+		this.captain = captain;
+	}
+	public City getCity() {
+		return city;
+	}
+	public void setCity(City city) {
+		this.city = city;
+	}
+	public Team(String fname, String coach, String captain, City city) {
+		super();
+		this.fname = fname;
+		this.coach = coach;
+		this.captain = captain;
+		this.city = city;
+	}
+	
+	
+	
+}
